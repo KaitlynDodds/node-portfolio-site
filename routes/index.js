81b3c497ad3bf98aa7f20c3bd857748062bf3d75
KaitlynDodds <kadodds.dev@kadodds.com>
2018-08-15 14:42:55 -0600

    basic routes set up

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,16 @@ router.get('/', (req, res) => {
 	res.render('home');
 });
 
+router.get('/about', (req, res) => {
+	res.send('about page');
+});
+
+router.get('/projects', (req, res) => {
+	res.send('projects page');
+});
+
+router.get('/project/:id', (req, res) => {
+	res.send(`project ${req.params.id}`);
+});
+
 module.exports = router;
