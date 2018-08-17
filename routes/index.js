5fb5da3a70e8d6659d53d36f45216e989a7045c0
KaitlynDodds <kadodds.dev@kadodds.com>
2018-08-17 09:52:59 -0600

    moved images to public dir, index page displaying project thumbnails

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
-const express = require('express');
-const router = express.Router();
+const express 		= require('express');
+const router 		= express.Router();
+
+const { projects } 	= require('../data.json');
+
+/* Routes
+***********/
 
 router.get('/', (req, res) => {
-	res.render('index');
+	res.render('index', { projects });
 });
 
 router.get('/about', (req, res) => {
